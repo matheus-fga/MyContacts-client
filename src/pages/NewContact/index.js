680ee81ae2caf9e10bb3bdebefa3c881277cffbf
Matheus Silva <matheussilva.fga@gmail.com>
2022-07-01 16:31:50 -0300

    Creating Button component

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -2,6 +2,7 @@ import PageHeader from '../../components/PageHeader';
 
 import Input from '../../components/Input';
 import Select from '../../components/Select';
+import Button from '../../components/Button';
 
 export default function NewContact() {
   return (
@@ -11,6 +12,8 @@ export default function NewContact() {
       <Select>
         <option value="Instagram">Instagram</option>
       </Select>
+      <Button type="button">Cadastrar</Button>
+      <Button type="button" disabled>Cadastrar</Button>
     </>
   );
 }
