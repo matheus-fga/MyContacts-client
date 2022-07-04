39bdaec6e9105f6c5993af610a6f64425ef0edd4
Matheus Silva <matheussilva.fga@gmail.com>
2022-07-04 15:15:14 -0300

    Creating Modal component

---
+++ src/components/Modal/index.js
@@ -0,0 +1,23 @@
+import { Overlay, Container, Footer } from './styles';
+
+import Button from '../Button';
+
+export default function Modal() {
+  return (
+    <Overlay>
+      <Container>
+        <h1>Modal</h1>
+        <p>
+          Corpo do Modal
+        </p>
+
+        <Footer>
+          <button type="button" className="cancel-button">
+            Cancelar
+          </button>
+          <Button type="button">Deletar</Button>
+        </Footer>
+      </Container>
+    </Overlay>
+  );
+}
