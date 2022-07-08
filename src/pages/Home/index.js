bb7e08452e3556397027b0de559f68181ee12a25
Matheus Silva <matheussilva.fga@gmail.com>
2022-07-08 15:06:35 -0300

    Creating contacts service to fetch API

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,7 +10,8 @@ import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 
 import Loader from '../../components/Loader';
-import delay from '../../utils/delay';
+
+import ContactsService from '../../services/ContactsService';
 
 export default function Home() {
   const [contacts, setContacts] = useState([]);
@@ -26,12 +27,10 @@ export default function Home() {
     async function loadContatcts() {
       try {
         setIsLoading(true);
-        const response = await fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`);
 
-        await delay();
+        const contactsList = await ContactsService.listContacts(orderBy);
 
-        const json = await response.json();
-        setContacts(json);
+        setContacts(contactsList);
       } catch (error) {
         console.log('error', error);
       } finally {
