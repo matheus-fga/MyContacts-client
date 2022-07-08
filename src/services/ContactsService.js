5fb933f9f2a5902f606fbd1082bbb137349ae544
Matheus Silva <matheussilva.fga@gmail.com>
2022-07-08 15:34:47 -0300

    Creating HttpClient

--- src/services/ContactsService.js
+++ src/services/ContactsService.js
@@ -1,8 +1,12 @@
-class ContactsService {
-  async listContacts(orderBy = 'asc') {
-    const response = await fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`);
+import HttpClient from './utils/HttpClient';
 
-    return response.json();
+class ContactsService {
+  constructor() {
+    this.httpClient = new HttpClient('http://localhost:3001');
+  }
+
+  async listContacts(orderBy = 'asc') {
+    return this.httpClient.get(`/contacts?orderBy=${orderBy}`);
   }
 }
 
