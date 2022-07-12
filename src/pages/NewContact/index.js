eac7cd58dfa34942f295d329c6b793247bfcd5da
Matheus Silva <matheussilva.fga@gmail.com>
2022-07-12 17:06:53 -0300

    Implementing contact creation

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,12 +1,32 @@
+/* eslint-disable react/jsx-no-bind */
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
+import ContactsService from '../../services/ContactsService';
 
 export default function NewContact() {
+  async function handleSubmit(formData) {
+    try {
+      const response = await ContactsService.createContact({
+        name: formData.name,
+        email: formData.email,
+        phone: formData.phone,
+        category_id: formData.categoryId,
+      });
+
+      console.log(response);
+    } catch {
+      alert('Ocorreu um erro ao cadastrar um contato!');
+    }
+  }
+
   return (
     <>
       <PageHeader title="Novo contato" />
 
-      <ContactForm buttonLabel="Cadastrar" />
+      <ContactForm
+        buttonLabel="Cadastrar"
+        onSubmit={handleSubmit}
+      />
     </>
   );
 }
