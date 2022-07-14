f42977330a5b1ee633dc0dda149ee41c651b9f4f
Matheus Silva <matheussilva.fga@gmail.com>
2022-07-14 17:08:16 -0300

    Loading contact data on edit page

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,12 +1,50 @@
+import { useEffect, useRef, useState } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
+import Loader from '../../components/Loader';
+
+import ContactsService from '../../services/ContactsService';
+import toast from '../../utils/toast';
 
 export default function EditContact() {
+  const [isloading, setIsLoading] = useState(true);
+  const contactFormRef = useRef(null);
+
+  const { id } = useParams();
+  const history = useHistory();
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contact = await ContactsService.getContactById(id);
+        contactFormRef.current.setFieldsValues(contact);
+        setIsLoading(false);
+      } catch {
+        history.push('/');
+        toast({
+          type: 'danger',
+          text: 'Contato não encontrado!',
+        });
+      }
+    }
+
+    loadContact();
+  }, [id, history]);
+
+  const handleSubmit = () => {};
+
   return (
     <>
+      <Loader isLoading={isloading} />
       <PageHeader title="Editar Contato" />
 
-      <ContactForm buttonLabel="Salvar Alterações" />
+      <ContactForm
+        ref={contactFormRef}
+        buttonLabel="Salvar Alterações"
+        onSubmit={handleSubmit}
+      />
     </>
   );
 }
